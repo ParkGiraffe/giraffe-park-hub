800590abcc97740744fd352ca94c62c830c3d348
ParkGiraffe <op5321@soongsil.ac.kr>
2023-04-19 16:38:05 +0900

    LinkButton 제작 중

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  background-color: cyan;
+  width: 260px;
+  height: 57px;
+  border-radius: 50px;
+`;
+
+const LinkButton = () => {
+  return <ButtonContainer></ButtonContainer>;
+};
+
+export default LinkButton;
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,11 @@
 import ParkText from "./ParkText";
 import Frame from "./Frame";
+import LinkButton from "./LinkButton";
 
 const Section = () => {
   return(
     <Frame>
+      <LinkButton />
       <ParkText>Blog Park</ParkText>
     </Frame>
   )
