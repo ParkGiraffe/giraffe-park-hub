1143d2d0a9e8da5409d8c51e08b684fbcd620ee2
ParkGiraffe <op5321@soongsil.ac.kr>
2023-05-02 15:13:57 +0900

    칸 간의 여백 수치 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const TitleConatiner = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 5%;
+  margin-top: 45px;
 `;
 
 const SignatureText = styled.text`
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,19 +4,24 @@ import styled from "styled-components";
 
 const FirstLine = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   /* background-color: pink; */
-  padding-left: 7%;
-  padding-right: 7%;
-  margin-top: 3%;
+  /* padding-left: 7%; */
+  /* padding-right: 7%; */
+  margin-top: 25px;
 `;
+
+const FirstLineBlock = styled.div`
+  width: 20px;
+`
+
 const SecondLine = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   /* background-color: cyan; */
-  padding-left: 22%;
-  padding-right: 22%;
-  margin-top: 2%;
+  /* padding-left: 22%;
+  padding-right: 22%; */
+  margin-top: 20px;
 `;
 
 const HomePage = () => {
@@ -25,11 +30,14 @@ const HomePage = () => {
       <Title />
       <FirstLine>
         <Section park={"Blog"} />
+        <FirstLineBlock/>
         <Section park={"Portfolio"} />
+        <FirstLineBlock/>
         <Section park={"GitHub"} />
       </FirstLine>
       <SecondLine>
         <Section park={"Gaming"} />
+        <FirstLineBlock />
         <Section park={"Character"} />
       </SecondLine>
     </>
